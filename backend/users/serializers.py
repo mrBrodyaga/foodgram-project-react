5a121cc9831d86d2bbb3b81f1a5612229d965616
mrBrodyaga <rukosuev1993@yandex.ru>
2021-10-16 14:46:35 +0700

    review 1

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -80,7 +80,8 @@ class SubscribeToSerializer(serializers.ModelSerializer):
 
     def get_recipes_count(self, obj):
         author_id = obj.id
-        count = Recipe.objects.filter(author_id=author_id).count()  # noqa: R504
+        count = Recipe.objects.filter(
+            author_id=author_id).count()  # noqa: R504
         return count  # noqa: R504
 
     def get_recipes(self, obj):
